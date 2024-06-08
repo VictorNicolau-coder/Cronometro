f76ada43e96568e7a89a0b63e26e7edcfc6a9b85
Victor Anderson <victoranderson1575@gmail.com>
2024-06-08 16:31:39 -0300

    mudança completa do site

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const audio = new Audio('/resource/song.mp3');
+audio.loop = true;
+
+window.addEventListener('load', () => {
+  var timer = 1746451839;
+  var flipdown = new FlipDown(timer, {
+    theme: "light",
+  })
+  .start()
+  .ifEnded(() => {
+    console.log('The countdown has ended!');
+  });
+
+  audio.play()
+  .catch(error => {
+    console.error('Erro ao reproduzir a música: ', error);
+  });
+});
